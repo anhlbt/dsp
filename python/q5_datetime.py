b082d5caeb5fe94e1c6a8c4b4cb35c35a5d9157e
Michelle Gill <github@michellelynngill.com>
2016-05-18 21:30:35 -0400

    Finished datetime in python section

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,22 @@
-# Hint:  use Google to find python function
+from datetime import datetime as dt
 
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+date_str = '%m-%d-%Y'
+(dt.strptime(date_stop, date_str) - dt.strptime(date_start, date_str)).days
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_str = '%m%d%Y'
+(dt.strptime(date_stop, date_str) - dt.strptime(date_start, date_str)).days
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+date_str = '%d-%b-%Y'
+(dt.strptime(date_stop, date_str) - dt.strptime(date_start, date_str)).days
